eb989f8e7307e8c620057363653de37d1e06df81
LAPTOP-BELQSNK3\31543 <315436432@qq.com>
2019-05-06 15:53:45 +0800

    加入网络判断工具类

--- lib_common/proguard-rules.pro
+++ lib_common/proguard-rules.pro
@@ -148,3 +148,20 @@ rx.internal.util.atomic.LinkedQueueNode consumerNode;
     <init>(java.lang.Throwable);
 }
 
+# Serializable
+# Explicitly preserve all serialization members. The Serializable interface
+# is only a marker interface, so it wouldn't save them.
+-keepclassmembers class * implements java.io.Serializable {
+     static final long serialVersionUID;
+     private static final java.io.ObjectStreamField[] serialPersistentFields;
+     private void writeObject(java.io.ObjectOutputStream);
+     private void readObject(java.io.ObjectInputStream);
+     java.lang.Object writeReplace();
+     java.lang.Object readResolve();
+ }
+
+# JavaBean
+#-keep public class yourBeanPackageName.**{*;}
+
+
+
---
+++ lib_common/src/main/java/com/leo/common/utils/NetUtil.java
@@ -0,0 +1,190 @@
+package com.leo.common.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.util.Enumeration;
+
+/**
+ * 
+ * @author LeoWang
+ * 
+ * @Package com.leo.common.utils
+ * 
+ * @Description 网络状态判断工具类
+ * 
+ * @Date 2019/5/6 15:45
+ * 
+ * @modify:
+ */
+public class NetUtil {
+    private static int NET_CNNT_BAIDU_OK = 1; // NetworkAvailable
+    private static int NET_CNNT_BAIDU_TIMEOUT = 2; // no NetworkAvailable
+    private static int NET_NOT_PREPARE = 3; // Net no ready
+    private static int NET_ERROR = 4; //net error
+    private static int TIMEOUT = 3000; // TIMEOUT
+
+
+    /**
+     * check NetworkAvailable
+     * @param context context
+     * @return 网络是否可用
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager manager = (ConnectivityManager) context.getApplicationContext().getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        if (null == manager)
+            return false;
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return null != info && info.isAvailable();
+    }
+
+    /**
+     * getLocalIpAddress
+     * @return Ip地址
+     */
+    public static String getLocalIpAddress() {
+        String ret = "";
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        ret = inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return ret;
+    }
+
+    /**
+     * 返回当前网络状态
+     *
+     * @param context context
+     * @return 网络状态
+     */
+    public static int getNetState(Context context) {
+        try {
+            ConnectivityManager connectivity = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivity != null) {
+                NetworkInfo networkinfo = connectivity.getActiveNetworkInfo();
+                if (networkinfo != null) {
+                    if (networkinfo.isAvailable() && networkinfo.isConnected()) {
+                        if (!connectionNetwork())
+                            return NET_CNNT_BAIDU_TIMEOUT;
+                        else
+                            return NET_CNNT_BAIDU_OK;
+                    } else {
+                        return NET_NOT_PREPARE;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return NET_ERROR;
+    }
+
+    /**
+     * ping "http://www.baidu.com"
+     * @return 是否可以连接
+     */
+    static private boolean connectionNetwork() {
+        boolean result = false;
+        HttpURLConnection httpUrl = null;
+        try {
+            httpUrl = (HttpURLConnection) new URL("http://www.baidu.com")
+                    .openConnection();
+            httpUrl.setConnectTimeout(TIMEOUT);
+            httpUrl.connect();
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != httpUrl) {
+                httpUrl.disconnect();
+            }
+            httpUrl = null;
+        }
+        return result;
+    }
+
+    /**
+     * check is3G
+     * @param context context
+     * @return 是否是3G
+     */
+    public static boolean is3G(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = null;
+        if (connectivityManager != null){
+            activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        return activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_MOBILE;
+    }
+
+    /**
+     * isWifi
+     * @param context context
+     * @return 是否是wifi
+     */
+    public static boolean isWifi(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = null;
+        if (connectivityManager != null){
+            activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        return activeNetInfo != null
+                && activeNetInfo.getType() == ConnectivityManager.TYPE_WIFI;
+    }
+
+    /**
+     * is2G
+     * @param context context
+     * @return 是否是2G
+     */
+    public static boolean is2G(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetInfo = null;
+        if (connectivityManager != null){
+            activeNetInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if (activeNetInfo != null
+                && (activeNetInfo.getSubtype() == TelephonyManager.NETWORK_TYPE_EDGE
+                || activeNetInfo.getSubtype() == TelephonyManager.NETWORK_TYPE_GPRS || activeNetInfo
+                .getSubtype() == TelephonyManager.NETWORK_TYPE_CDMA)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  is wifi on
+     */
+    public static boolean isWifiEnabled(Context context) {
+        ConnectivityManager mgrConn = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        TelephonyManager mgrTel = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        return ((mgrConn.getActiveNetworkInfo() != null && mgrConn
+                .getActiveNetworkInfo().getState() == NetworkInfo.State.CONNECTED) || mgrTel
+                .getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS);
+    }
+}
